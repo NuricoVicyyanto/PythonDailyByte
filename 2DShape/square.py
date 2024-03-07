4afb241b70ca3700edf5af0dfc8c53197e8f9229
nuricovicyyanto <nuricovicyyanto2@gmail.com>
2024-03-07 14:47:24 +0700

    add : passwordGenerator

---
+++ 2DShape/square.py
@@ -0,0 +1,9 @@
+# input value
+width1 = int(input("Enter width 1 :"))
+width2 = int(input("Enter width 2 :"))
+
+# rectangle formula
+rectangle = width1 * width2
+
+# result
+print("Area square is :", rectangle)
